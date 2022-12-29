6ff0f22fb0b0721a5c78c0dbe17c6544bd6f3c47
OlieHimmars <109204093+OlieHimmars@users.noreply.github.com>
2022-12-29 18:39:06 +0200

    task2

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,3 +1,26 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const startBtn = document.querySelector('button');
+//const stopBtn = document.querySelector("button [data-stop]");  чому так не працює?
+let timerColor = null;
+startBtn.nextElementSibling.disabled = true;
+
+startBtn.addEventListener('click', onStart);
+startBtn.nextElementSibling.addEventListener('click', onStop);
+
+function onStart() {
+    document.body.style.background = getRandomHexColor();
+    timerColor = setInterval(() => {
+        document.body.style.background = getRandomHexColor();
+    }, 1000);
+    startBtn.disabled = true;
+    startBtn.nextElementSibling.disabled = false;
+};
+
+function onStop() {
+    clearInterval(timerColor);
+    startBtn.disabled = false;
+    startBtn.nextElementSibling.disabled = true;
+};
--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,16 +1,44 @@
 import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
+import { Ukrainian } from "flatpickr/dist/l10n/uk.js"   //????
 import Notiflix from 'notiflix';//не обов'язково
-
 
 const options = {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
-  onClose(selectedDates) {
-    console.log(selectedDates[0]);
+  disableMobile: true,
+    onClose(selectedDates) {
+        chosenTime = selectedDates[0].getTime();
+        onCount = chosenTime - fp.now.getTime();
+    if (onCount > 0) {
+        startBtn.disabled = false;
+    } else{
+        Notiflix.Report.failure('Please choose a date in the future', '', 'OK');
+    };
   },
+};
+
+const startBtn = document.querySelector("button[type='button']");
+const myInput = document.querySelector("#datetime-picker");
+const fp = flatpickr(myInput, options);
+
+
+
+let daysOnCount = document.querySelector("span[data-days]");
+let hoursOnCount = document.querySelector('span[data-hours]');
+let minOnCount = document.querySelector('span[data-minutes]');
+let secOnCount = document.querySelector('span[data-seconds]');
+
+let onCount = 0;
+startBtn.disabled = true;
+startBtn.addEventListener('click', onStart);
+myInput.addEventListener('submit', options.onClose)
+
+
+function onStart(e) {
+    setInterval(convertMs(onCount), 1000);
 };
 
 function convertMs(ms) {
@@ -21,17 +49,28 @@ function convertMs(ms) {
   const day = hour * 24;
 
   // Remaining days
-  const days = Math.floor(ms / day);
+    let days = Math.floor(ms / day);
+    if (days < 10) {
+        days = '0' + days;
+    };
+    daysOnCount.innerHTML = days;
   // Remaining hours
-  const hours = Math.floor((ms % day) / hour);
+    let hours = Math.floor((ms % day) / hour);
+    if (hours < 10) {
+        hours = '0' + hours;
+    };
+    hoursOnCount.innerHTML = hours;
   // Remaining minutes
-  const minutes = Math.floor(((ms % day) % hour) / minute);
+    let minutes = Math.floor(((ms % day) % hour) / minute);
+    if (minutes < 10) {
+        minutes = '0' + minutes;
+    };
+    minOnCount.innerHTML = minutes;
   // Remaining seconds
-  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+    let seconds = Math.floor((((ms % day) % hour) % minute) / second);
+    if (seconds < 10) {
+        seconds = '0' + seconds;
+    }
+    secOnCount.innerHTML = seconds;
 
-  return { days, hours, minutes, seconds };
 }
-
-console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
-console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
-console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
