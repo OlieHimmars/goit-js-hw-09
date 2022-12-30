4c5d0f389ca59a983c79f0126d08789bb990d8e0
OlieHimmars <109204093+OlieHimmars@users.noreply.github.com>
2022-12-30 17:07:52 +0200

    add4

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -34,30 +34,20 @@ let hoursOnCount = document.querySelector('span[data-hours]');
 let minOnCount = document.querySelector('span[data-minutes]');
 let secOnCount = document.querySelector('span[data-seconds]');
 
-//let onCount = 0;
 startBtn.disabled = true;
 startBtn.addEventListener('click', onStart);
-//myInput.addEventListener('submit', options.onClose)
 
 function convertMs(ms) {
-  // Number of milliseconds per unit of time
     ms -= 1000;
   const second = 1000;
   const minute = second * 60;
   const hour = minute * 60;
   const day = hour * 24;
-
-  // Remaining days
    counter.days = Math.floor(ms / day);
-  // Remaining hours
    counter.hours = Math.floor((ms % day) / hour);
-  // Remaining minutes
    counter.minutes = Math.floor(((ms % day) % hour) / minute);
-  // Remaining seconds
    counter.seconds = Math.floor((((ms % day) % hour) % minute) / second);
-
- // = { days, hours, minutes, seconds };
-}
+};
 
 function addLeadingZero(value) {
     return String(value).padStart(2, '0');
@@ -65,13 +55,11 @@ function addLeadingZero(value) {
 
 function onStart(e) {
     setInterval( updateClock(counter), 1000 );
-
 };
 
 function updateClock({ days, hours, minutes, seconds }) {
-    //onCount = newDate - fp.now.getTime();
     daysOnCount.innerHTML = addLeadingZero(days);
     hoursOnCount.innerHTML = addLeadingZero(hours);
     minOnCount.innerHTML = addLeadingZero(minutes);
     secOnCount.innerHTML = addLeadingZero(seconds);
-}
+};
